2cf67dea1f093812aa979a6c34f670242e9bc2ae
dec3ptions <105021570+dec3ptions@users.noreply.github.com>
2025-01-05 19:30:19 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,3 @@
--- :)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/Sources/Willow"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/Sources/Dec3ptions"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/Sources/AntiLag"))()
