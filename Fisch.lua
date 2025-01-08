ce08013db99fb3fb4d96bb4a24b9f008bbd266ff
dec3ptions <105021570+dec3ptions@users.noreply.github.com>
2025-01-08 04:48:21 +0000

    Update and rename Main.lua to Fisch.lua

---
+++ Fisch.lua
@@ -0,0 +1,16 @@
+local settings = {
+    RenderingEnabled = true, -- Default: true
+    PerformanceBlackScreen = true,  -- Default: true
+    DecalsEnabled = false, -- Default: false
+    ShadowsEnabled = false, -- Default: false
+
+    AutoShakeDelay = 0.02, -- Default: 0.02
+    FreezeCharacter = true,  -- Default: true
+    TeleportLocation = nil, -- Default: nil
+    
+    Webhook = "", -- Set to webhook url
+    WebhookUpdateDelay = 300  -- Default: 300
+}
+
+local dec3ptions = loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/versions/v0.0.4"))()
+dec3ptions.Main(settings)
--- Main.lua
+++
@@ -1,4 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/Sources/Dec3ptions"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/Sources/Willow"))()
-wait(60)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/hacks/Sources/AntiLag"))()
