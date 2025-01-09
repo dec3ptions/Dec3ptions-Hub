b9c44941a10d472f04b91c4720e7d53e29eed746
dec3ptions <105021570+dec3ptions@users.noreply.github.com>
2025-01-09 23:45:19 +0000

    v1.2.0

--- Fisch.lua
+++ Fisch.lua
@@ -1,19 +1,21 @@
 local settings = {
     RenderingEnabled = false, -- Default: false
     PerformanceBlackScreen = false,  -- Default: false
+    RemoveGui = true, -- Default: truw
     DecalsEnabled = false, -- Default: false
     ShadowsEnabled = false, -- Default: false
 
+    RodName = "Rod Of The Forgotten Fang", -- Default: "Rod Of The Forgotten Fang"
     AutoShakeDelay = 0.02, -- Default: 0.02
     FreezeCharacter = true,  -- Default: true
     TeleportLocation = nil, -- Default: nil
     
     Webhook = "", -- Set to webhook url
-    WebhookUpdateDelay = 600  -- Default: 600
+    WebhookUpdateDelay = 300  -- Default: 300
 }
 
-local dec3ptions = loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/main/Versions/v1.1.1"))()
-dec3ptions.Main(settings)
+local dec3ptions = loadstring(game:HttpGet("https://raw.githubusercontent.com/dec3ptions/Dec3ptions-Hub/refs/heads/main/Versions/v1.2.0"))()
+dec3ptions.Initialise(settings)
 
 -- Made by Dec3ptions
 
